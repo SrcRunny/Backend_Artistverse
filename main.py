7604231d8129dd4d5971f35ec1c8cb76d52bdc9f
SrcRunny <Run34047@gmail.com>
2024-03-21 04:47:47 +0700

    flask framework setup

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+app.config.from_object(__name__)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+@app.route('/', methods=['GET'])
+def greeting():
+    return ("Hello World!")
+
+@app.route('/test', methods=['GET'])
+def homepage():
+    return ("This is the test page!")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
